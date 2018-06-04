72c551698d548b1cf496f19a0ce7d42757c56f14
liwei2016011717 <2335644296@qq.com>
2018-06-04 14:06:31 +0800

    更新

--- 课程练习/LS25/事件练习/test.js
+++ 课程练习/LS25/事件练习/test.js
@@ -2,18 +2,18 @@ window.onload=function(e){
     //console.log("window.load");
     var div1=document.getElementById("div1")
     var div2=document.getElementById("div2")
-    div1.onclick=function(e){
-        console.log(e);
-        console.log(e.clientX,e.clientY);
-       console.log(e.target);
-       console.log(this);
-      console.log(e.type);
-       console.log(e);
-       console.log(e.__proto__);
-       console.log(e.__proto__.__proto__);
-       console.log(e.__proto__.__proto__.__proto__);
-       console.log(e.__proto__.__proto__.__proto__.__proto__);
-   }
+//     div1.onclick=function(e){
+//         console.log(e);
+//         console.log(e.clientX,e.clientY);
+//        console.log(e.target);
+//        console.log(this);
+//       console.log(e.type);
+//        console.log(e);
+//        console.log(e.__proto__);
+//        console.log(e.__proto__.__proto__);
+//        console.log(e.__proto__.__proto__.__proto__);
+//        console.log(e.__proto__.__proto__.__proto__.__proto__);
+//    }
 
 
     //DOM0级事件监听
@@ -29,12 +29,30 @@ window.onload=function(e){
 
 
     //DOM2级事件监听
-        function clickHandler(e){
-            console.log(e.target);
-        }
-        div1.addEventListener("click",clickHandler);
-        div2.addEventListener("click",clickHandler);
-        document.addEventListener("click",clickHandler);
-        window.addEventListener("click",clickHandler);
-}
-  
+        // function clickHandler(e){
+        //     console.log(e.target);
+        // }
+        // div1.addEventListener("click",clickHandler);
+        // div2.addEventListener("click",clickHandler);
+        // document.addEventListener("click",clickHandler);
+        // window.addEventListener("click",clickHandler);
+
+
+    //事件流
+    div1.addEventListener("click",function (e) {
+        console.log("div1 click",this,e.target);
+        e.stopPropagation();
+    },false);//第3个参数可以不写，默认为false
+
+    div2.addEventListener("click",function (e) {
+        console.log("div2 click",this,e.target);
+    },false);
+
+    document.addEventListener("click",function (e) {
+        console.log("document click",this,e.target);
+    },false);
+
+    document.body.addEventListener("click",function (e) {
+        console.log("body click",this,e.target);
+    },true); //依次输出b 1 2 d
+}//target(触发的最精确的节点)和thisc
